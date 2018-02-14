697a3119a3c380ae35cc26d968c2fd81122d3ec6
Bishawjit1993 <Bishawjit1993@gmail.com>
2018-02-14 17:18:07 -0600

    Method twenty three

--- src/practice/PracticeOne.java
+++ src/practice/PracticeOne.java
@@ -1,7 +1,21 @@
 package practice;
 
 public class PracticeOne {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+		System.out.println("QUE-23:" + getFifthToTenthChar("Thisisastring"));
+	}
+
+/**
+ * que-23: write a method that take one string input and return fifth to tenth character as a string.
+ * parameter: String text
+ * returnType: String
+ * servingBucket: variable
+ */
+	public static String getFifthToTenthChar(String text) {
+		String result = "";
+		for (int i = 4; i <= 9; i++) {
+			result = result + text.charAt(i);
+		}
+		return result;
+	}
 }
