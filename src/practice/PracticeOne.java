38d645c7c68759946d69da8ae700e6c95ec68bb9
Bishawjit1993 <Bishawjit1993@gmail.com>
2018-02-14 18:31:18 -0600

    Method twenty eight

--- src/practice/PracticeOne.java
+++ src/practice/PracticeOne.java
@@ -8,6 +8,25 @@ public class PracticeOne {
 		System.out.println("QUE-24:" + getAllWord("this is a string"));
 		System.out.println("QUE-26:" + getCount("this is a string", "a"));
 		System.out.println("QUE-27:" + getCharacterCount("This is a string", 'a'));
+		System.out.println("QUE-28:" + getTrueFalse("this is ", "This"));
+	}
+	/**
+	 * que-28: write a method that take one string & one word input and return true if that sentence contains given word otherwise return false.
+	 * parameter: String text, String word
+	 * returnType: boolean
+	 * servingBucket: variable
+	 */
+	public static boolean getTrueFalse(String text, String word) {
+		boolean result = false;
+		text = text.toLowerCase();
+		word = word.toLowerCase();
+		String[] wordArray = text.split(" ");
+		for (int i = 0; i < wordArray.length; i++) {
+			if (wordArray[i].matches(word)) {
+				result = true;
+			}
+		}
+		return result;
 	}
 	/**
 	 * que-27: write a method that take one string & one character input and return the count of that given word.
@@ -20,12 +39,13 @@ public class PracticeOne {
 		text = text.toLowerCase();
 		character = Character.toLowerCase(character);
 		for (int i = 0; i < text.length(); i++) {
-			if(text.charAt(i) == character) {
+			if (text.charAt(i) == character) {
 				count = count + 1;
 			}
 		}
 		return count;
 	}
+
 	/**
 	 * que-26: write a method that take one string & one word input and return the count of that given word.
 	 * parameter: String textOne, String textTwo
